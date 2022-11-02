ea86bd74c12a554a32f68fbcedf940d6693eae73
mao <1296193245@qq.com>
2022-11-02 22:16:17 +0800

    jwt_spring_boot_starter

---
+++ tools-jwt/src/main/java/com/example/tools_jwt/entity/Token.java
@@ -0,0 +1,19 @@
+package com.example.tools_jwt.entity;
+
+/**
+ * Project name(项目名称)：jwt_spring_boot_starter
+ * Package(包名): com.example.tools_jwt.entity
+ * Class(类名): Token
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/11/2
+ * Time(创建时间)： 22:15
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Token
+{
+
+}
